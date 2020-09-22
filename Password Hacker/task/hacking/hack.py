91be59286f9bf9a233510c505bb9894d58f5841e
merlin2181 <merlin2181@gmail.com>
2020-09-21 19:15:55 -0700

    Stage 3 of 5 of Password Hacker project from JetBrains Academy
    - Uses a dictionary and the itertools.product() method to find the password to hack into the server.

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -2,6 +2,7 @@ import argparse
 import itertools
 import socket
 
+
 pass_hacker = argparse.ArgumentParser(description='An automated password hacker')
 pass_hacker.add_argument("hostname", type=str, help='the IP address or domain name of the site you are trying to hack')
 pass_hacker.add_argument("port", type=int, help='the port number of the site you are trying to hack')
@@ -10,25 +11,18 @@ args = pass_hacker.parse_args()
 hacker = socket.socket()
 address = (args.hostname, args.port)
 
-# Create a list with a-z and 0-9
-char_list = [chr(i) for i in range(97, 123)] + [chr(i) for i in range(48, 58)]
-flag = True
-i = 1
 with hacker as h:
     h.connect(address)
-    while flag:
-        combinations = itertools.product(char_list, repeat=i)
-        for item in combinations:
-            data = ''.join(item)  # change tuple to string
-            data = data.encode()  # encode data string
-            h.send(data)  # send data string
-            rec_data = h.recv(1024)
+    with open('passwords.txt') as f:
+        for line in f:
+            password = line.strip()
+            temp = map(''.join, itertools.product(*((char.upper(), char.lower()) if char.isalpha()
+                                                    else char for char in password)))
+            for item in temp:
+                h.send(item.encode())
+                rec_data = h.recv(1024)
+                if rec_data == b'Connection success!':
+                    print(item)
+                    break
             if rec_data == b'Connection success!':
-                print(data.decode())
-                flag = False
                 break
-            elif rec_data == b'Too many attempts':
-                print('Too many attempts')
-                flag = False
-                break
-        i += 1
